0001321294804126a123907bb6f09d2c60f0f3d5
Richard Clubb <rich.clubb@hidglobal.com>
2025-03-21 21:20:50 +0000

    Renamed to cpp secrecy

--- tests/unit/include/test_cpp_secrecy.h
+++ tests/unit/include/test_cpp_secrecy.h
@@ -1,13 +1,7 @@
-#ifndef __TEST_C_SECRECY_TESTS__
+#ifndef __TEST_CPP_SECRECY_TESTS__
 
-#define __TEST_C_SECRECY_TESTS__
+#define __TEST_CPP_SECRECY_TESTS__
 
-void run_int_suite(void);
-void run_float_suite(void);
 
-void run_string_suite(void);
-void run_array_suite(void);
-
-void run_vector_suite(void);
 
 #endif
